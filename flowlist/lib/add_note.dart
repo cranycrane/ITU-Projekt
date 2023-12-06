520fe72274e01aaa857bfcfc8a3f75fe44edfcc5
xteich02 <xteich02@stud.fit.vut.cz>
2023-12-06 19:40:53 +0100

    Uprava chyby velikosti screenu

--- flowlist/lib/add_note.dart
+++ flowlist/lib/add_note.dart
@@ -67,156 +67,146 @@ class _NewEntryPageState extends State<NewEntryPage> {
 
   
 
-  @override
+ @override
   Widget build(BuildContext context) {
     final double statusBarHeight = MediaQuery.of(context).padding.top;
-   // final double bottomInset = MediaQuery.of(context).viewInsets.bottom; // výška klávesnice nebo dalšího dolního obsahu
-    //final double screenHeight = MediaQuery.of(context).size.height - statusBarHeight - bottomInset;
     final double keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
-    final DateFormat formatter = DateFormat('EEEE d.M.yyyy', 'cs_CZ');
+    final double screenHeight = MediaQuery.of(context).size.height;
 
-    String formattedDate = formatter.format(selectedDate).toUpperCase();
-    //final double bottomBarHeight = kBottomNavigationBarHeight;
-
-    // Celková výška, kterou je třeba zabrat, aby se obsah nezobrazoval pod klávesnicí
-    //final double bodyHeight = screenHeight - bottomBarHeight;
-
-    bool isKeyboardVisible = MediaQuery.of(context).viewInsets.bottom != 0;
+    String formattedDate =
+        DateFormat('EEEE d.M.yyyy', 'cs_CZ').format(selectedDate).toUpperCase();
 
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween, // Zarovnání obsahu na obě strany
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             IconButton(
-              icon: Icon(Icons.chevron_left, color: Colors.black), // Icon barva nastavena na černou
+              icon: Icon(Icons.chevron_left, color: Colors.black),
               onPressed: () => _changeDay(-1),
             ),
             Text(
-              formattedDate, // Použití pomocné funkce pro zobrazení data
-              style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black), // Text barva nastavena na černou
+              formattedDate,
+              style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
             ),
             IconButton(
-              icon: Icon(Icons.chevron_right, color: Colors.black), // Icon barva nastavena na černou
+              icon: Icon(Icons.chevron_right, color: Colors.black),
               onPressed: () => _changeDay(1),
             ),
           ],
         ),
         centerTitle: true,
-        elevation: 0, // Odstranění stínu AppBaru
-        backgroundColor: Colors.transparent, // Transparentní AppBar
+        elevation: 0,
+        backgroundColor: Colors.transparent,
       ),
 
-      
       resizeToAvoidBottomInset: true,
 
-
       body: SingleChildScrollView(
-        physics: keyboardHeight > 0 ? AlwaysScrollableScrollPhysics() : NeverScrollableScrollPhysics(),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.stretch, // Přidáno pro zarovnání
-              children: <Widget>[
-                SizedBox(height: statusBarHeight),
-                TextField(
-                  controller: _firstController,
-                  decoration: InputDecoration(
-                    labelText: 'První položka',
-                    border: OutlineInputBorder(),
-                    fillColor: Colors.grey[200],
-                    filled: true,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              SizedBox(height: statusBarHeight),
+              TextField(
+                controller: _firstController,
+                decoration: InputDecoration(
+                  labelText: 'První položka',
+                  border: OutlineInputBorder(),
+                  fillColor: Colors.grey[200],
+                  filled: true,
+                ),
+                maxLines: 5,
+              ),
+              SizedBox(height: 8),
+              TextField(
+                controller: _secondController,
+                decoration: InputDecoration(
+                  labelText: 'Druhá položka',
+                  border: OutlineInputBorder(),
+                  fillColor: Colors.grey[200],
+                  filled: true,
+                ),
+                maxLines: 5,
+              ),
+              SizedBox(height: 8),
+              TextField(
+                controller: _thirdController,
+                decoration: InputDecoration(
+                  labelText: 'Třetí položka',
+                  border: OutlineInputBorder(),
+                  fillColor: Colors.grey[200],
+                  filled: true,
+                ),
+                maxLines: 5,
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Flexible(
+                    child: Text(
+                      'Jak bys ohodnotil svůj den?',
+                      style: TextStyle(fontSize: 16),
+                    ),
                   ),
-                  maxLines: 5,
-                ),
-                SizedBox(height: 8),
-                TextField(
-                  controller: _secondController,
-                  decoration: InputDecoration(
-                    labelText: 'Druhá položka',
-                    border: OutlineInputBorder(),
-                    fillColor: Colors.grey[200],
-                    filled: true,
+                  SizedBox(
+                    width: 80,
+                    child: TextField(
+                      controller: _ratingController,
+                      decoration: InputDecoration(
+                        hintText: '/10',
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
                   ),
-                  maxLines: 5,
-                ),
-                SizedBox(height: 8),
-                TextField(
-                  controller: _thirdController,
-                  decoration: InputDecoration(
-                    labelText: 'Třetí položka',
-                    border: OutlineInputBorder(),
-                    fillColor: Colors.grey[200],
-                    filled: true,
+                ],
+              ),
+              SizedBox(height: 16),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Akce pro smazání dat
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.grey,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        padding: EdgeInsets.symmetric(vertical: screenHeight * 0.02), // Upravit podle potřeby
+                      ),
+                      child: Text('SMAZAT'),
+                    ),
                   ),
-                  maxLines: 5,
-                  
-                ),
-                SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Flexible(
-                      child: Text(
-                        'Jak bys ohodnotil svůj den?',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 80, // Nastavení pevné šířky pro textové pole
-                      child: TextField(
-                        controller: _ratingController,
-                        decoration: InputDecoration(
-                          hintText: '/10',
-                          border: OutlineInputBorder(),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Akce pro uložení dat
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.red,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5),
                         ),
-                        keyboardType: TextInputType.number,
+                        padding: EdgeInsets.symmetric(vertical: screenHeight * 0.02), // Upravit podle potřeby
                       ),
+                      child: Text('ULOŽIT'),
                     ),
-                  ],
-                ),
-                SizedBox(height: 16),
-                Row( // Tlačítka vedle sebe s mezerou
-                  children: <Widget>[
-                    Expanded(
-                      child: ElevatedButton(
-                        onPressed: () {
-                          // Akce pro smazání dat
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.grey, // Barva tlačítka SMAZAT
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(5), // Bez zaoblení
-                          ),
-                          padding: EdgeInsets.symmetric(vertical: 16),
-                        ),
-                        child: Text('SMAZAT'),
-                      ),
-                    ),
-                    SizedBox(width: 8), // Mezera mezi tlačítky
-                    Expanded(
-                      child: ElevatedButton(
-                        onPressed: () {
-                          // Akce pro uložení dat
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.red, // Barva tlačítka ULOŽIT
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(5), // Bez zaoblení
-                          ),
-                          padding: EdgeInsets.symmetric(vertical: 16),
-                        ),
-                        child: Text('ULOŽIT'),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
+      ),
+
       bottomNavigationBar: BottomAppBar(
         shape: CircularNotchedRectangle(),
         notchMargin: 6.0,
@@ -232,7 +222,7 @@ class _NewEntryPageState extends State<NewEntryPage> {
               icon: Icon(Icons.search, color: _selectedIndex == 1 ? Colors.red : Colors.grey),
               onPressed: () => _onItemTapped(1),
             ),
-            SizedBox(width: 48), // Prostor pro Floating Action Button
+            SizedBox(width: 48),
             IconButton(
               icon: Icon(Icons.notifications_none, color: _selectedIndex == 2 ? Colors.red : Colors.grey),
               onPressed: () => _onItemTapped(2),
@@ -244,18 +234,16 @@ class _NewEntryPageState extends State<NewEntryPage> {
           ],
         ),
       ),
-      floatingActionButton :isKeyboardVisible
-      ? null
 
-      
-      : FloatingActionButton(
-        backgroundColor: Colors.red,
-        child: Icon(Icons.add),
-        onPressed: () {
-          // Akce pro FloatingActionButton
-        },
-
-      ),
+      floatingActionButton: keyboardHeight > 0
+          ? null
+          : FloatingActionButton(
+              backgroundColor: Colors.red,
+              child: Icon(Icons.add),
+              onPressed: () {
+                // Akce pro FloatingActionButton
+              },
+            ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
